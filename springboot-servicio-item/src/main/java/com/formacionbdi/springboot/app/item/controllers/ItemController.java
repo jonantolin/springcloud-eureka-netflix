3c140bc302ef8ca47352909bbbce828bd2c25c1d
jonantolin <jonantolin@gmail.com>
2020-06-17 17:15:11 +0200

    agregado spring cloud config server y spring boot actuator

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -4,8 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +21,24 @@ import com.formacionbdi.springboot.app.item.model.Item;
 import com.formacionbdi.springboot.app.item.service.IItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	//Una alternativa a Qualifier es usar la anotacion @Primary en el servicio que queramos utilizar
 	// @Qualifier("serviceRestTemplate") //usando servicio cliente RestTemplate
 	// @Qualifier("serviceFeign") // usando servicio cliente Feign
 	@Autowired
 	@Qualifier("serviceFeign") // usando servicio cliente Feign
 	private IItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String textoConfig;
 	
 	//@GetMapping("/items")
 	@GetMapping("")
@@ -60,6 +74,23 @@ public class ItemController {
 		response.put("mensaje", "producto no encontrado");
 		
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		log.info(textoConfig);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("mensaje", textoConfig);
+		json.put("port", puerto);
+		
+		if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("desarrollo")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 	
 	/*
