98889e9c54beca56171f5472feb5a0162e34b34c
Jon Antolin <Usuario@192.168.1.103>
2021-03-01 13:14:55 +0100

    Cambiado parametro de busqueda por nombre para el cliente

--- springboot-servicio-usuarios/src/main/java/com/formacion/springboot/app/usuarios/models/dao/UsuarioDAO.java
+++ springboot-servicio-usuarios/src/main/java/com/formacion/springboot/app/usuarios/models/dao/UsuarioDAO.java
@@ -2,7 +2,9 @@ package com.formacion.springboot.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.formacion.springboot.app.usuarios.models.entity.Usuario;
 
@@ -13,7 +15,8 @@ import com.formacion.springboot.app.usuarios.models.entity.Usuario;
 public interface UsuarioDAO extends PagingAndSortingRepository<Usuario, Long> {
 
 	//Metodo por convencion de Spring Data (findBy)
-	public Usuario findByUsername(String username);
+	@RestResource(path="buscar-username")
+	public Usuario findByUsername(@Param("username") String username);
 	
 	//Metodo 'tradicional', consulta referenciada a objetos (no a tablas)
 	@Query("select u from Usuario u where u.username=?1")
